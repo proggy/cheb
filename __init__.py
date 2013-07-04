4e17eb06daa3f6bc40de925df88d63a7069640d9
Daniel Jung <d.jung@jacobs-university.de>
2013-07-04 18:04:33 +0900

    Added license information and .gitignore.

--- __init__.py
+++ __init__.py
@@ -4,20 +4,35 @@
 Chebychev polynomials play a central role within the kernel polynomial
 method [1].
 
-[1] Weiße et al., Rev. Mod. Phys. 78, 275 (2006)
-
-Author:  Daniel Jung, Jacobs University Bremen, Bremen, Germany.
-Contact: d.jung@jacobs-university.de
-
-To do:
---> let poly1d object have a display hook for pretty printing in IPython 
-    notebook
-"""
+[1] Weiße et al., Rev. Mod. Phys. 78, 275 (2006)"""
 __created__ = '2013-07-04'
 __modified__ = '2013-07-04'
-import numpy
+"""
+To do:
+--> let poly1d object have a display hook for pretty printing in IPython
+    notebook
+"""
 
-# if module "frog" exists, use it, otherwise, create a dummy decorator
+
+# Copyright (C) 2013 Daniel Jung
+# Contact: d.jung@jacobs-university.de
+#
+# This program is free software; you can redistribute it and/or modify it
+# under the terms of the GNU General Public License as published by the Free
+# Software Foundation; either version 2 of the License, or (at your option)
+# any later version.
+#
+# This program is distributed in the hope that it will be useful, but WITHOUT
+# ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+# FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License for
+# more details.
+#
+# You should have received a copy of the GNU General Public License along
+# with this program; if not, write to the Free Software Foundation, Inc.,
+# 59 Temple Place, Suite 330, Boston, MA 02111-1307 USA.
+
+
+import numpy
 try:
   from frog import Frog
 except ImportError:
